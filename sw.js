6a96b9b3a73ff66809b29479eb71688e3676986e
富田直樹 <tomitanaoki@MacBook-Air-2.local>
2026-08-14 17:02:01 +0900

    Add Service Worker cache clearing logic to force update latest script

---
+++ sw.js
@@ -0,0 +1,26 @@
+// Service Worker: キャッシュを即座にクリアして最新版を適用する
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
